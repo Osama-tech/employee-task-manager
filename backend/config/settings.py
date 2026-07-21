7040d9029743d13e12a3c03441cdd1de8b4784bf
Osama Elkot <osama@circiutiq.com>
2026-07-21 02:55:54 -0400

    chore(api): configure Django REST Framework

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+
     "apps.users.apps.UsersConfig",
     "apps.departments.apps.DepartmentsConfig",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
