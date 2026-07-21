8591639447028abe30917585edc0a584608db63c
Osama Elkot <osama@circiutiq.com>
2026-07-21 02:56:08 -0400

    feat(users): associate users with departments

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "department",
+        "is_active",
+    )
+    search_fields = (
+        "username",
+        "email",
+    )
+
+    list_filter = (
+        "role",
+        "department",
+        "is_active",
+    )
+
+    ordering = (
+        "username",
+    )
--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 from .choices import Role
+from apps.departments.models import Department
 
 class User(AbstractUser):
     role = models.CharField(
@@ -8,6 +10,14 @@ class User(AbstractUser):
         choices=Role.choices,
         default=Role.EMPLOYEE,
     )
-    
+
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.PROTECT,
+        related_name="employees",
+        null=True,
+        blank=True
+    )
+
     def __str__(self):
         return self.username
