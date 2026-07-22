0e725d1643172a2494ff0e76c6abf85d6a668409
Osama Elkot <osama@circiutiq.com>
2026-07-21 22:45:31 -0400

    feat(auth): implement JWT authentication and current user endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,12 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path("api/users/", include("apps.users.urls")),
+    
     path("admin/", admin.site.urls),
 
     path(
         "api/departments/",
         include("apps.departments.urls"),
+    ),
+
+    path(
+        "api/auth/login/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+
+    path(
+        "api/auth/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
     ),
 ]
